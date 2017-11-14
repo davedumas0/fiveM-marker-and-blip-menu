aa8b26ee6116a4e76b5aa931510f2b092afa0ebe
davedumas0 <davedumas0@yahoo.com>
2017-11-13 19:57:29 -0800

    Update server script.lua

--- server script.lua
+++ server script.lua
@@ -18,14 +18,14 @@ AddEventHandler('markers', function(markBlipType, markType, X, Y, Z)
  local whileloop = "\n    while true do"
  local citwait = "\n      Citizen.Wait(0)"
  local drawmark = "\n     DrawMarker("
- local searater = ", "
+ local separater = ", "
  local closedrawmark = ", 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 1.5, 2.5, 200, 233, 0, 150, 0, 0, 2, 0, 0, 0, false )"
  local endwhileloop = "\n    end"
  local closethread = "\n end)\n"
 -----------------------------------------------------------------------
 -----------------------------------------------------------------------
  file = io.open("C:\\server-data\\resources\\test3\\menu3.lua", "a")
- file:write(createthread..setlocalvar..appendnumtolocalvar..a1B..addblip..X..searater..Y..searater..Z..closeline..setmarkblipsprite..setlocalvar2..appendnumtolocalvar..num521..closeline..whileloop..citwait..drawmark..markType..searater..X..searater..Y..searater..Z..closedrawmark..endwhileloop..closethread)
+ file:write(createthread..setlocalvar..appendnumtolocalvar..a1B..addblip..X..separater..Y..separater..Z..closeline..setmarkblipsprite..setlocalvar2..appendnumtolocalvar..num521..closeline..whileloop..citwait..drawmark..markType..separater..X..separater..Y..separater..Z..closedrawmark..endwhileloop..closethread)
  file:close()
  end)
  
